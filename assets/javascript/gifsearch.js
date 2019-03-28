39569635a041f9f4666f13b1997a950b5effdb00
Ethan <ebonsignori@gmail.com>
2019-03-28 13:32:04 -0400

    search / add new buttons / play and pause gifs

--- assets/javascript/gifsearch.js
+++ assets/javascript/gifsearch.js
@@ -1,7 +1,8 @@
-let topics = ['game of thrones', 'stranger things', 'spongebob squarepants', 'last week tonight', 'seinfeld', 'curb your enthusiasm']
-let searchButtonContainer = $('#search-button-container')
+let topics = ['game of thrones', 'stranger things', 'orange is the new black', 'spongebob squarepants', 'the good place', 'the haunting of hill house', 'last week tonight', 'seinfeld', 'curb your enthusiasm', 'the office', 'breaking bad']
 
+// Create buttons based on what is in the topics array
 let generateButtons = () => {
+  let searchButtonContainer = $('#search-button-container')
   searchButtonContainer.empty()
   for (let i = 0; i < topics.length; i++) {
     let button = $(`<button type="button" class="btn btn-info search-button" data-value="${topics[i]}">`)
@@ -11,16 +12,31 @@ let generateButtons = () => {
   }
 }
 
-$(document).on('click', '#submit-button', function (event) {
-  event.preventDefault()
-
-  let newButton = $('#new-show').val()
-  // if (topics.indexOf(newButton) === -1) {
-  topics.push(newButton)
-  // }
-  generateButtons()
+// Add new button to the list
+$(document).on('click', '#submit-button', function () {
+  let newButton = $('#new-show').val().trim().toLowerCase()
+  checkString(newButton)
 })
 
+let checkString = (text) => {
+  let alert = $('.alert')
+  if (text === '') {
+    alert.html(`<strong>Some text is required!</strong>`)
+    alert.addClass('show')
+    return
+  }
+  if (topics.indexOf(text) >= 0) {
+    console.log('already have that button')
+    alert.html(`<strong>You already have a button for that!</strong>`)
+    alert.addClass('show')
+    return
+  }
+  alert.removeClass('show')
+  topics.push(text)
+  generateButtons()
+}
+
+// Search and append gifs
 $(document).on('click', '.search-button', function () {
   let gifDisplay = $('#gif-display')
   gifDisplay.empty()
@@ -36,21 +52,43 @@ $(document).on('click', '.search-button', function () {
     console.log(response)
     for (let i = 0; i <= limit; i++) {
       let path = response.data[i]
-      let gifStill = path.images.fixed_height_still.url
-      let gifAnimated = path.images.fixed_height.url
+      let still = path.images.fixed_height_still.url
+      let animated = path.images.fixed_height.url
+      let original = path.images.original.url
+      let title = path.title
       let rating = path.rating
       let newGif = $(`
         <figure>
         <figcaption>rating: ${rating}</figcaption>
-        <img src="${gifStill}" 
-        data-still="${gifStill}" 
-        data-animate="${gifAnimated}" 
+        <img src="${still}" 
+        data-still="${still}" 
+        data-animate="${animated}" 
         data-state="still"
         class="gif">
+        <figcaption><a href="${original}" download="${title}>Download</a>
         </figure`)
       gifDisplay.append(newGif)
     }
   })
+})
+
+// Pause and play gifs when clicked on
+$(document).on('click', '.gif', function () {
+  let state = $(this).attr('data-state')
+  if (state === 'still') {
+    $(this).attr('src', $(this).attr('data-animate'))
+    $(this).attr('data-state', 'animate')
+  } else {
+    $(this).attr('src', $(this).attr('data-still'))
+    $(this).attr('data-state', 'still')
+  }
+})
+
+// Prevent page from reloading when user presses the enter key
+$('#search-form').submit(function () {
+  let newButton = $('#new-show').val().trim().toLowerCase()
+  checkString(newButton)
+  return false
 })
 
 generateButtons()
